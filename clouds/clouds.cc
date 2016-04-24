e87a91b26299742e8a16c4acfc5b5142e954198e
Matthias Puech <puech@cs.mcgill.ca>
2016-04-24 22:04:14 +0200

    clouds: reduce main file to bare minimum, for Tom

--- clouds/clouds.cc
+++ clouds/clouds.cc
@@ -22,116 +22,91 @@
 // 
 // See http://creativecommons.org/licenses/MIT/ for more information.
 
-#include "clouds/cv_scaler.h"
 #include "clouds/drivers/codec.h"
-#include "clouds/drivers/debug_pin.h"
-#include "clouds/drivers/debug_port.h"
 #include "clouds/drivers/system.h"
 #include "clouds/drivers/version.h"
-#include "clouds/dsp/granular_processor.h"
-#include "clouds/meter.h"
-#include "clouds/resources.h"
-#include "clouds/settings.h"
-#include "clouds/ui.h"
-
-// #define PROFILE_INTERRUPT 1
+#include "clouds/drivers/leds.h"
+#include "clouds/drivers/switches.h"
+#include "clouds/drivers/adc.h"
 
 using namespace clouds;
-using namespace stmlib;
 
-GranularProcessor processor;
-Codec codec;
-DebugPort debug_port;
-CvScaler cv_scaler;
-Meter meter;
-Settings settings;
-Ui ui;
+Leds leds;
+Switches switches;
+Adc adc;
 
-// Pre-allocate big blocks in main memory and CCM. No malloc here.
-uint8_t block_mem[118784];
-uint8_t block_ccm[65536 - 128] __attribute__ ((section (".ccmdata")));
-
-int __errno;
+int counter;
 
 // Default interrupt handlers.
 extern "C" {
+  void NMI_Handler() { }
+  void HardFault_Handler() { while (1); }
+  void MemManage_Handler() { while (1); }
+  void BusFault_Handler() { while (1); }
+  void UsageFault_Handler() { while (1); }
+  void SVC_Handler() { }
+  void DebugMon_Handler() { }
+  void PendSV_Handler() { }
 
-void NMI_Handler() { }
-void HardFault_Handler() { while (1); }
-void MemManage_Handler() { while (1); }
-void BusFault_Handler() { while (1); }
-void UsageFault_Handler() { while (1); }
-void SVC_Handler() { }
-void DebugMon_Handler() { }
-void PendSV_Handler() { }
-
-}
-
-extern "C" {
-
-void SysTick_Handler() {
-  ui.Poll();
-  if (settings.freshly_baked()) {
-    if (debug_port.readable()) {
-      uint8_t command = debug_port.Read();
-      uint8_t response = ui.HandleFactoryTestingRequest(command);
-      debug_port.Write(response);
+  // called every 1ms
+  void SysTick_Handler() {
+    counter++;
+    // a little LED animation for fun
+    for(int i=0; i<4; i++) {
+      leds.set_status(i, (counter+i*64) & 255, (counter+i*64+128) & 255);
     }
+    leds.Write();
   }
 }
 
-}
-
+// called every time the codec needs one buffer of data
 void FillBuffer(Codec::Frame* input, Codec::Frame* output, size_t n) {
-#ifdef PROFILE_INTERRUPT
-  TIC
-#endif  // PROFILE_INTERRUPT
-  cv_scaler.Read(processor.mutable_parameters());
-  processor.Process((ShortFrame*)input, (ShortFrame*)output, n);
-  meter.Process(processor.parameters().freeze ? output : input, n);
-#ifdef PROFILE_INTERRUPT
-  TOC
-#endif  // PROFILE_INTERRUPT
+
+  // read the ADC and get the value of the Position pot/CV
+  adc.Convert();
+  float pos = adc.float_value(ADC_POSITION_POTENTIOMETER_CV);
+
+  // for each sample
+  while (n--) {
+    // the left output gets attenuated by the position
+    output->l = input->l * pos;
+    // the right output takes the ring-modulation of the two inputs
+    output->r = (input->l * input->r) >> 16;
+    // advance the buffers by one position
+    output++;
+    input++;
+  }
 }
 
 void Init() {
+
+  // start the timer that calls SysTick_Handler (see system.h)
   System sys;
-  Version version;
-
   sys.Init(true);
-  version.Init();
-
-  // Init granular processor.
-  processor.Init(
-      block_mem, sizeof(block_mem),
-      block_ccm, sizeof(block_ccm));
-
-  settings.Init();
-  cv_scaler.Init(settings.mutable_calibration_data());
-  meter.Init(32000);
-  ui.Init(&settings, &cv_scaler, &processor, &meter);
-
-  bool master = !version.revised();
-  if (!codec.Init(master, 32000)) {
-    ui.Panic();
-  }
-  if (!codec.Start(32, &FillBuffer)) {
-    ui.Panic();
-  }
-  if (settings.freshly_baked()) {
-#ifdef PROFILE_INTERRUPT
-    DebugPin::Init();
-#else
-    debug_port.Init();
-#endif  // PROFILE_INTERRUPT
-  }
   sys.StartTimers();
+
+  // later versions of Clouds have slight hardware difference; this is
+  // to differentiate them
+  Version version;
+  version.Init();
+  bool master = !version.revised();
+
+  // initialize the codec at 96kHz, with a buffer size of 32 samples
+  Codec codec;
+  codec.Init(master, 96000);
+  codec.Start(32, &FillBuffer);
+
+  // configure and initialize the internal ADC (for CV and pots) and
+  // LED driver.
+  leds.Init();
+  adc.Init();
 }
 
+// this is the function that is called on startup
 int main(void) {
+  // initialize and configure all the devices
   Init();
+  // and then do nothing (the timers will trigger the appropriate functions)
   while (1) {
-    ui.DoEvents();
-    processor.Prepare();
   }
 }
